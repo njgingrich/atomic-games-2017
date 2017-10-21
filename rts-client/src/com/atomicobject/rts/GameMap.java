dedfff34f4bc89a37353d31cf4cdb4c06ff14bd7
Nathan <njgingrich@gmail.com>
2017-10-21 11:20:58 -0400

    Add enemy detection and attempts to gather resources

--- rts-client/src/com/atomicobject/rts/GameMap.java
+++ rts-client/src/com/atomicobject/rts/GameMap.java
@@ -1,25 +1,48 @@
 package com.atomicobject.rts;
 
 import com.atomicobject.rts.model.Tile;
+import com.atomicobject.rts.model.Unit;
 
 public class GameMap {
     private static int ROWS = 30;
-    public Tile[][] rows;
+    public Tile[][] tiles;
+    public Unit[][] enemies;
 
     public GameMap() {
-        rows = new Tile[ROWS * 2][ROWS * 2];
+        tiles = new Tile[ROWS * 2][ROWS * 2];
+        enemies = new Unit[ROWS * 2][ROWS * 2];
     }
 
-    public Tile[] row(int row) {
-        return rows[row];
+    public Tile[] tileRow(int row) {
+        return tiles[row];
     }
 
-    public Tile get(int row, int col) {
-        return rows[ROWS + row][ROWS + col];
+    public Unit[] unitRow(int row) {
+        return enemies[row];
     }
 
-    public void put(Long row, Long col, Tile t) {
+    public Tile getTile(int row, int col) {
+        return tiles[ROWS + row][ROWS + col];
+    }
+
+    public void putTile(Long row, Long col, Tile t) {
         // Base is actually at [ROWS,ROWS]
-        rows[ROWS + row.intValue()][ROWS + col.intValue()] = t;
+        tiles[ROWS + row.intValue()][ROWS + col.intValue()] = t;
+    }
+
+    public Unit getEnemy(int row, int col) {
+        return enemies[ROWS + row][ROWS + col];
+    }
+
+    public void putEnemy(Long row, Long col, Unit u) {
+        enemies[ROWS + row.intValue()][ROWS + col.intValue()] = u;
+    }
+
+    public static String getDirection(Long rowFrom, Long colFrom, Long rowTo, Long colTo) {
+        if ((rowFrom - rowTo == 1) && (colFrom - colTo == 0)) return "N";
+        else if ((rowFrom - rowTo == -1) && (colFrom - colTo == 0)) return "S";
+        else if ((rowFrom - rowTo == 0) && (colFrom - colTo == 1)) return "E";
+        else if ((rowFrom - rowTo == 0) && (colFrom - colTo == -1)) return "W";
+        else return "N"; // ew
     }
 }
